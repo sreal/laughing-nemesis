1446e168483ff986ba6975599409530bbae06e91
Simon Eames <simon.richard.eames@gmail.com>
2013-07-14 16:17:48 -0700

    added different directive testing styles

--- js/app.js
+++ js/app.js
@@ -3,10 +3,76 @@ AdtApp.Modules = {};
 
 AdtApp.Modules
   .controllers = angular.module('controllers', [])
+
   .controller('AppCtrl',[
     /*****/ '$scope',
     function($scope) {
       $scope._evaluated = true;
     }]
   ) // AppCtrl
+  .controller('ModuleOneCtrl',[
+    /*****/ '$scope',
+    function($scope) {
+      $scope.value = "controller-value";
+    }]
+  ) // ModuleOneCtrl
+;
+AdtApp.Modules
+  .directives = angular.module('directives', [])
+  .directive('basicDirective', function() {
+    return {
+      restrict: "E",
+      template:"<div name='template-element'><div>",
+      link: function(scope, elem, attr) { /*do nothing*/}
+    }
+  })
+  .directive('containedControllerDirective', function() {
+    return {
+      restrict: "E",
+      scope: {
+        value: "@"
+      },
+      replace: true,
+      controller: ['$scope', function($scope){ $scope.value = 'controller-value' }] ,
+      template:"<div name='template-element'><div>",
+      link: function(scope, elem, attr) {
+        var el = elem.find('div')[0];
+        el.innerHTML = scope.value
+      }
+    }
+  }) // containedControllerDirective
+  .controller('SameModuleCtrl', [
+    '$scope',
+    function($scope) { $scope.value = 'controller-value' }
+  ])
+  .directive('sameModuleDirective', function() {
+    return {
+      restrict: "E",
+      scope: {
+        value: "@"
+      },
+      replace: true,
+      controller: 'SameModuleCtrl',
+      template:"<div name='template-element'><div>",
+      link: function(scope, elem, attr) {
+        var el = elem.find('div')[0];
+        el.innerHTML = scope.value
+      }
+    }
+  }) // sameModuleDirective
+  .directive('differentModuleDirective', function() {
+    return {
+      restrict: "E",
+      scope: {
+        value: "@"
+      },
+      replace: true,
+      controller: 'ModuleOneCtrl',
+      template:"<div name='template-element'><div>",
+      link: function(scope, elem, attr) {
+        var el = elem.find('div')[0];
+        el.innerHTML = scope.value
+      }
+    }
+  });// sameModuleDirective
 ;
--- js/app.spec.js
+++ js/app.spec.js
@@ -4,7 +4,7 @@ describe("app: ", function() {
     expect(undefined).not.toBeDefined();
   });
 
-  describe("controller: ", function () {
+  describe("controller: ", function() {
     var $scope, crtl;
 
     beforeEach( function() {
@@ -21,6 +21,149 @@ describe("app: ", function() {
       expect($scope._evaluated).toBeTruthy();
     });
 
-  }); // controllers
+  }); // controller
+
+  describe("directive: ", function() {
+    var $scope, $compile, crtl;
+
+
+    describe("basic directive: ", function() {
+      var element;
+
+      beforeEach( function() {
+        module('directives');
+        inject(function(_$rootScope_, _$compile_) {
+          $scope = _$rootScope_.$new(); // inject the testing singleton
+          $compile = _$compile_
+        });
+      });
+
+      beforeEach(function() {
+        // wrap directive in div and use fully qualified html
+        element = angular.element("<div><basic-directive></basic-directive></div>");
+        var linkFunction = $compile(element); // compile html
+        linkFunction($scope); // link to scope
+        $scope.$apply(); // run the scope
+      });
+
+      it("loads from template", function() {
+        var el = element.find('template-element');
+        expect(el).toBeDefined();
+      });
+
+    }); // basic directive
+
+    describe("container directive: ", function() {
+      var element;
+
+      beforeEach( function() {
+        module('directives');
+        inject(function(_$rootScope_, _$compile_) {
+          $scope = _$rootScope_.$new(); // inject the testing singleton
+          $compile = _$compile_
+        });
+      });
+
+      beforeEach(function() {
+        // wrap directive in div and use fully qualified html
+        element = angular.element("<div><contained-controller-directive></contained-controller-directive></div>");
+        var linkFunction = $compile(element); // compile html
+        linkFunction($scope); // link to scope
+        $scope.$apply(); // run the scope
+      });
+
+      it("loads from template", function() {
+        var el = element.find('div')[0];
+        var name = el.attributes['name'].value;
+        expect(el).toBeDefined();
+        expect(name).toBe('template-element');
+      });
+
+      it("loads value from controller scope", function() {
+        var el = element.find('div')[0];
+        var name = el.attributes['name'].value;
+        expect(el).toBeDefined();
+        expect(name).toBe('template-element');
+        expect(el.textContent).toBe('controller-value');
+      });
+
+    }); // contained directive
+
+    describe("same module directive: ", function() {
+      var element;
+
+      beforeEach( function() {
+        module('directives');
+        inject(function(_$rootScope_, _$compile_) {
+          $scope = _$rootScope_.$new(); // inject the testing singleton
+          $compile = _$compile_
+        });
+      });
+
+      beforeEach(function() {
+        // wrap directive in div and use fully qualified html
+        element = angular.element("<div><same-module-directive></same-module-directive></div>");
+        var linkFunction = $compile(element); // compile html
+        linkFunction($scope); // link to scope
+        $scope.$apply(); // run the scope
+      });
+
+      it("loads from template", function() {
+        var el = element.find('div')[0];
+        var name = el.attributes['name'].value;
+        expect(el).toBeDefined();
+        expect(name).toBe('template-element');
+      });
+
+      it("loads value from controller scope", function() {
+        var el = element.find('div')[0];
+        var name = el.attributes['name'].value;
+        expect(el).toBeDefined();
+        expect(name).toBe('template-element');
+        expect(el.textContent).toBe('controller-value');
+      });
+
+    }); // same module directive
+
+    describe("different module directive: ", function() {
+      var element;
+
+      beforeEach( function() {
+        module('controllers');
+        module('directives');
+        inject(function(_$rootScope_, _$compile_) {
+          $scope = _$rootScope_.$new(); // inject the testing singleton
+          $compile = _$compile_
+        });
+      });
+
+      beforeEach(function() {
+        // wrap directive in div and use fully qualified html
+        element = angular.element("<div><different-module-directive></different-module-directive></div>");
+        var linkFunction = $compile(element); // compile html
+        linkFunction($scope); // link to scope
+        $scope.$apply(); // run the scope
+      });
+
+      it("loads from template", function() {
+        var el = element.find('div')[0];
+        var name = el.attributes['name'].value;
+        expect(el).toBeDefined();
+        expect(name).toBe('template-element');
+      });
+
+      it("loads value from controller scope", function() {
+        var el = element.find('div')[0];
+        var name = el.attributes['name'].value;
+        expect(el).toBeDefined();
+        expect(name).toBe('template-element');
+        expect(el.textContent).toBe('controller-value');
+      });
+
+    }); // different module directive
+
+
+  }); // directive
+
 
 });// describe app
